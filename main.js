fc51c15a0b344b86e2faa67633377fdd5931f6c3
monett010 <acts2018+gh@gmail.com>
2025-09-24 13:24:50 -0400

    Fixed it so that gamenum is an argument for the Word class

--- main.js
+++ main.js
@@ -41,14 +41,15 @@ class Game {
 }
 
 class Word {
-    constructor (word){
+    constructor (word, gamenum){
         this.word = word;
+        this.gamenum = gamenum;
     }
 
     // boolean that returns true if the answers array has the word in it, 
     // false if it doesn't
-    async isAnswer (gamenum) {
-        const g = new Game(gamenum);
+    async isAnswer () {
+        const g = new Game(this.gamenum);
         const game_answers = await g.getAnswers();
         return game_answers.includes(this.word);
     }
@@ -59,8 +60,8 @@ class Word {
     }
 
     // returns true if the word is a pangram
-    async isPangram (gamenum) {
-        const g = new Game(gamenum);
+    async isPangram () {
+        const g = new Game(this.gamenum);
         const letters_ = await g.getLetters();
         const word_ = this.word.toUpperCase();
 
@@ -69,26 +70,26 @@ class Word {
 
         let intersection = letters.intersection(word);
 
-        if (await this.isAnswer(gamenum) === true && intersection.size === 7) {  
+        if (await this.isAnswer() === true && intersection.size === 7) {  
             return true;
         } else {
             return false;
         }
     }
 
-    async calculatePoints (gamenum) {
+    async calculatePoints () {
 
         // if the word is 4 letters, return 1 point
-        if (this.wordLength() === 4 && await this.isAnswer(gamenum)) {
+        if (this.wordLength() === 4 && await this.isAnswer()) {
             return 1;
         }
         // if the word is a pangram, return word length + 7
-        else if (this.wordLength() > 4 && await this.isPangram(gamenum) && await this.isAnswer(gamenum)) {
+        else if (this.wordLength() > 4 && await this.isPangram() && await this.isAnswer()) {
             return this.wordLength() + 7;
         }
         // if the word is greater than 4 letters but not a pangram,
         // return word length
-        else if (this.wordLength() > 4 && await this.isPangram(gamenum) === false && await this.isAnswer(gamenum)) {
+        else if (this.wordLength() > 4 && await this.isPangram() === false && await this.isAnswer()) {
             return this.wordLength();
         }
         //if the word is less than 4 letters or not an answer, return 0

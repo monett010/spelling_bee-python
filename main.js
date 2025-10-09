155637548de5156b630b6075921d739da9fb47a5
monett010 <acts2018+gh@gmail.com>
2025-10-09 15:48:28 -0400

    Fixed getGames() to reference its own class

--- main.js
+++ main.js
@@ -52,9 +52,7 @@ class Game {
 
     async getGames() {
         const games_list = document.getElementById("games_list")
-        const g = new Game("all")
-
-        const games = await g.fetchData (this.game_url + "all")
+        const games = await this.fetchData (this.game_url + "all")
 
         for (let x=0; x<games.length; x++) {
             const game_link = `<a href='spelling_bee.html?game=${games[x]}'>${games[x]}</a>`

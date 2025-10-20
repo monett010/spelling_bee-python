5ed98b08bb9de657f8f51136e19250d23d882ba0
monett010 <acts2018+gh@gmail.com>
2025-10-20 12:02:36 -0400

    Removed .git from showing up in games endpoint

--- Controller.py
+++ Controller.py
@@ -71,6 +71,6 @@ class ReadGameFiles ():
         for g in gamesdir:
             if (os.path.isdir(self.gamesdir_path + "/" + g)):
                 games.append(g)
-                
+        games.remove(".git")
         games.sort()
         return games

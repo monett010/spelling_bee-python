215d5103452ea04c1e55283cfe33fd1dc5844049
monett010 <acts2018+gh@gmail.com>
2025-12-16 10:45:13 -0500

    Removed setTimeout() from genius popup

--- main.js
+++ main.js
@@ -234,7 +234,7 @@ class GameGUI {
                 this.updatePointsEarned(await current_word.calculatePoints());
                 this.updatePointsEarnedTextBox(await current_word.calculatePoints());
                 setTimeout(()=>{this.updateProgressBar(points_earned, total_game_points)}, 750);
-                setTimeout(()=>{this.checkIfGenius(points_earned, total_game_points)},1000);
+                this.checkIfGenius(points_earned, total_game_points);
             } 
         //delete the word from the text input
         text_input.value = "";

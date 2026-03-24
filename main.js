7a55a3d9678668fc82ec919e9b5e0f904efc55db
monett010 <acts2018+gh@gmail.com>
2026-03-24 12:16:08 -0400

    Replaced game selector <ul> with <select> bar on index.html

--- main.js
+++ main.js
@@ -55,13 +55,23 @@ class Game {
         }
     }
 
+    // async getGames() {
+    //     const games_list = document.getElementById("games_list")
+    //     const games = await this.fetchData (this.game_url + "all")
+
+    //     for (let x=0; x<games.length; x++) {
+    //         const game_link = `<a href='spelling_bee.html?game=${games[x]}'>${games[x]}</a>`
+    //         games_list.innerHTML += `<li>${game_link}</li>`
+    //     }
+    // }
+
     async getGames() {
         const games_list = document.getElementById("games_list")
         const games = await this.fetchData (this.game_url + "all")
 
         for (let x=0; x<games.length; x++) {
             const game_link = `<a href='spelling_bee.html?game=${games[x]}'>${games[x]}</a>`
-            games_list.innerHTML += `<li>${game_link}</li>`
+            games_list.innerHTML += `<option value=${games[x]}>${games[x]}</option>`
         }
     }
 }
